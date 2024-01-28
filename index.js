b24fd1d7bd14ba14e5696651a83c00fdf5513794
Sean Gregory <57275515+seangregoryv8@users.noreply.github.com>
2024-01-27 20:29:26 -0500

    Fixed the bug with formatting

--- index.js
+++ index.js
@@ -8,37 +8,35 @@ async function initMap()
     map = new Map(document.getElementById("map"), {
     center: { lat: 45.3032, lng: -73.3315 },
     zoom: 8,
-    geocoder = new google.maps.Geocoder()
+    geocoder: new google.maps.Geocoder()
     });
 
-  map.addListener('click', function(event) 
+    map.addListener('click', function(event) 
     {
-      if (marker){
-        marker.setPosition(event.latLng);
-        
+        if (marker)
+            marker.setPosition(event.latLng);
+        else
+        { 
+            marker = new google.maps.Marker(
+            {
+                position: event.latLng,
+                map: map,
+                icon: {
+                    url : './icon/news_icon.png',
+                    scaledSize : new google.maps.Size(50, 50)
+                }
+            })
 
-      } else { 
-        marker = new google.maps.Marker({
-        position: event.latLng,
-        map: map,
-        icon: {
-          url : './icon/news_icon.png',
-          scaledSize : new google.maps.Size(50, 50)
+            geocoder.geocode(
+            {
+                'latLng' : event.latLng
+            }, (results, status) =>
+            {
+                if (status == google.maps.GeocoderStatus.OK || results[0])
+                    alert(results[0].formatted_address);
+            })
         }
-      })
-
-        geocoder.geocode({
-          'latLng' : event.latLng
-        }, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            if (results[0]) {
-              alert(results[0].formatted_address);
-            }
-          }
-      }
-    )
-  }
-
+    })
 }
 
 initMap();
