f4477e0c0ec31b35fb80ed4c8e926da679558b03
Zachary Therrien <ZTherrien.work@gmail.com>
2024-01-27 21:32:47 -0500

    I sold my soul to GNews API, please don't take 73$ worth of DQ

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let url = "https://gnews.io/api/v4/search?q=";
+const url = "https://gnews.io/api/v4/search?q=";
 let country = "";
 const API_KEY = "&apikey=87752405435ed2dd3fef1c0267ae5da2";
-
 
 country = "Canada"
 
 let query = url + country + API_KEY;
 
-fetch(query)
-    .then(function (response) {
-        // alert("oh no");
-        // console.log(response);
-        return response.json;
-    })
-    .then(function (data) {
-        let articles = data.articles;
+let list = document.getElementById("results");
+document.getElementById("findMe").addEventListener("click", articleSearch);
 
-        for (i = 0; i < articles.length; i++) {
-            console.log(articles[i]);
-        }
-    });
+let articles = [];
+
+function articleSearch(){
+    fetch(query)
+        .then(function (response) {
+            // alert("oh no");
+            // console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            articles = data.articles;
+            //console.log(articles);
+            //console.log(articles);
+            
+            //Populate the list
+            for (let i = 0; i < articles.length; i++) {
+                //console.log(articles[i]);
+                let article = document.createElement("li");
+                article.innerHTML = articles[i]['title'];
+                list.appendChild(article);
+                validateArticles();
+            }
+        })
+        .catch(function(){
+            console.error("oooppss");
+            alert();
+        });
+}
+
+function validateArticles(){
+
+}
