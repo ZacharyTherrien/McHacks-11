23fca3a91cf661bf35266b81b5e12d7d983ee128
mimi717 <marie-christine.sapoundjieva@mail.mcgill.ca>
2024-01-27 20:33:40 -0500

    Adding alert for city name

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ async function initMap()
 {
     const { Map } = await google.maps.importLibrary("maps");
 
+    let geocoder = new google.maps.Geocoder();
     map = new Map(document.getElementById("map"), {
     center: { lat: 45.3032, lng: -73.3315 },
     zoom: 8,
-    geocoder: new google.maps.Geocoder()
     });
 
     map.addListener('click', function(event) 
     {
+
         if (marker)
             marker.setPosition(event.latLng);
+
         else
         { 
             marker = new google.maps.Marker(
@@ -26,6 +28,7 @@ async function initMap()
                     scaledSize : new google.maps.Size(50, 50)
                 }
             })
+          }
 
             geocoder.geocode(
             {
@@ -35,7 +38,6 @@ async function initMap()
                 if (status == google.maps.GeocoderStatus.OK || results[0])
                     alert(results[0].formatted_address);
             })
-        }
     })
 }
 
